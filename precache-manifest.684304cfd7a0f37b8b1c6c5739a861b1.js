26169f029b2db265fe60ce123fe62351de7c1ea6
vasanth-kumar-m-y <vasanthgowda047@gmail.com>
2020-10-09 11:11:03 +0530

    Updates

---
+++ precache-manifest.684304cfd7a0f37b8b1c6c5739a861b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "366776d169e70889b770b66be909fdd3",
+    "url": "/vasanth-kumar-m-y/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b31c0b6edd72925cdc0e",
+    "url": "/vasanth-kumar-m-y/monsters-rolodex/static/css/main.a2764d4b.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/vasanth-kumar-m-y/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vasanth-kumar-m-y/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b31c0b6edd72925cdc0e",
+    "url": "/vasanth-kumar-m-y/monsters-rolodex/static/js/main.74254050.chunk.js"
+  },
+  {
+    "revision": "a97653a0be1010226ca3",
+    "url": "/vasanth-kumar-m-y/monsters-rolodex/static/js/runtime-main.46080572.js"
+  }
+]);
